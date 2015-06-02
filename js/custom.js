5bda797f9507f7ad3225945fe3ad2bc3d86353bb
ianbogda <ianbogda@gmail.com>
2015-06-02 14:28:51 +0200

    Initial

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(function() {
+	window.scrollReveal = new scrollReveal();
+	"use strict";
+	
+	// PreLoader
+	$(window).load(function() {
+		$(".loader").fadeOut(400);
+	});
+
+	// Backstretchs
+	$("#header").backstretch("images/3.jpg");
+	$("#services").backstretch("images/3.jpg");
+		
+    
+});
